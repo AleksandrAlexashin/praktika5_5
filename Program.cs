a0d5b6cbf0cad9f7bd2c9a57b659fe18e1f2e1ab
AleksandrAlexashin <sashokmetall@mail.ru>
2021-12-11 00:30:04 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika5_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите элемент массива N ");
+           int n= Convert.ToInt32(Console.ReadLine());
+
+            int[,] mass5 = new int[n,n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    
+                    mass5[i, j] = 1 - (i + j) % 2;
+                    Console.Write(" "+mass5[i, j] + " ");
+                   
+                }
+                             
+
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
